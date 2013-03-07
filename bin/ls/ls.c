5650472925bd7f313537f20d55937ba8b81c4be5
Jonathan Anderson <jonathan@freebsd.org>
2013-03-07 00:04:08 +0000

    Try out a (very, very simple) TESLA assertion in bin/ls.
    [git-p4: depot-paths = "//depot/projects/ctsrd/tesla/src/": change = 222685]

--- bin/ls/ls.c
+++ bin/ls/ls.c
@@ -67,6 +67,10 @@ __FBSDID("$FreeBSD: head/bin/ls/ls.c 242840 2012-11-09 20:19:56Z peter $");
 #include <signal.h>
 #endif
 
+#ifdef TESLA
+#include "tesla-macros.h"
+#endif
+
 #include "ls.h"
 #include "extern.h"
 
@@ -567,6 +571,10 @@ traverse(int argc, char *argv[], int options)
 static void
 display(const FTSENT *p, FTSENT *list, int options)
 {
+#ifdef	TESLA
+	TESLA_GLOBAL(since(called(main), called(traverse)));
+#endif
+
 	struct stat *sp;
 	DISPLAY d;
 	FTSENT *cur;
