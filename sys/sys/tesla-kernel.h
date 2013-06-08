c7c3f72f43c9ef767982c3f67b7cbc76a8f6ee0a
Robert Watson <bb+lists.freebsd.perforce@cyrus.watson.org>
2013-06-08 17:09:41 +0000

    Add a new tesla-kernel.h that will have useful FreeBSD kernel-specific TESLA definitions, such as TESLA_SYSCALL(), TESLA_VM_FAULT(), etc.
    Use this in ffs_vnops.c.
    
    [git-p4: depot-paths = "//depot/projects/ctsrd/tesla/src/": change = 229510]

---
+++ sys/sys/tesla-kernel.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2013 Robert N. M. Watson
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+ * ("CTSRD"), as part of the DARPA CRASH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _SYS_TESLA_KERNEL_H_
+#define	_SYS_TESLA_KERNEL_H_
+
+/*
+ * FreeBSD kernel-specific TESLA macros.
+ */
+
+/*
+ * Un-protyped functions that we care about.
+ *
+ * XXXRW: Solution is actually to prototype them properly.
+ */
+extern void syscall(void);
+
+/*
+ * Convenient assertion wrappers for various scopes.
+ */
+#define	TESLA_SYSCALL(x)	TESLA_WITHIN(syscall, x)
+#define	TESLA_VM_FAULT(x)	TESLA_WITHIN(vm_fault, x)
+
+#endif /* _SYS_TESLA_KERNEL_H_ */
