823b1aaa86b7aaeb4951b3bec60ffd181dfa9f91
Jonathan Anderson <jonathan@freebsd.org>
2013-08-22 14:29:03 +0000

    Pull in latest TESLA parts (commit db6ceb) from GitHub.
    This includes a very large change to how libtesla works; you will need to update TESLA tools to use this!
    
    [git-p4: depot-paths = "//depot/projects/ctsrd/tesla/src/": change = 422201]

--- sys/contrib/tesla/include/tesla.h
+++ sys/contrib/tesla/include/tesla.h
@@ -39,6 +39,10 @@
 #ifndef	TESLA_H
 #define	TESLA_H
 
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
 /**
  * API for programmers who want to use TESLA in their code.
  *
@@ -166,6 +170,10 @@ struct __tesla_automaton_usage;
 /** In an explicit automata description, return this to say "we're done". */
 struct __tesla_automaton_description*	__tesla_automaton_done();
 
+#ifndef __unused
+#define __unused
+#endif
+
 inline struct __tesla_automaton_usage*
 __tesla_struct_uses_automaton(__unused const char *automaton,
 	__unused __tesla_locality *loc, ...)
@@ -233,5 +241,7 @@ __tesla_struct_uses_automaton(__unused const char *automaton,
 #endif	/* __TESLA_ANALYSER__ */
 
 /** @} */
+
+__END_DECLS
 
 #endif	/* TESLA_H */
