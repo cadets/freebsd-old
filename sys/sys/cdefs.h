aeca9bfe6678252cda173b9fae42b1fabd454a73
Khilan Gudka <novazeta@FreeBSD.org>
2014-02-19 14:47:02 +0000

    Change definition of __weak_reference (when using clang) to use attributes rather than inline assembly. This enables the IR to understand weak references. Submitted by: Khilan Gudka (novazeta)
    [git-p4: depot-paths = "//depot/projects/ctsrd/tesla/src/": change = 1191164]

--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -508,6 +508,10 @@
 	extern __typeof (sym) aliassym __attribute__ ((__alias__ (#sym)))
 #endif
 #ifdef __STDC__
+#ifdef __clang__
+#define	__weak_reference(sym,alias)	\
+	extern __typeof (sym) alias __attribute__ ((weak, __alias__ (#sym)))
+#endif /* __clang__ */
 #define	__weak_reference(sym,alias)	\
 	__asm__(".weak " #alias);	\
 	__asm__(".equ "  #alias ", " #sym)
