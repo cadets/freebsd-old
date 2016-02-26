bd04e09e6bb84f170c0623d7e1d05781406d743a
Jonathan Anderson <jonathan.anderson@ieee.org>
2016-02-25 21:38:52 -0330

    Squeeze another few bytes out of boot2.

--- sys/boot/i386/boot2/boot2.c
+++ sys/boot/i386/boot2/boot2.c
@@ -155,7 +155,11 @@ void exit(int);
 static void load(void);
 static int parse(void);
 static int dskread(void *, unsigned, unsigned);
+#ifdef TIGHT_SQUEEZE
+#define printf(...)
+#else
 static void printf(const char *,...);
+#endif
 static void putchar(int);
 static int drvread(void *, unsigned, unsigned);
 static int keyhit(unsigned);
@@ -562,6 +566,7 @@ error:
     return -1;
 }
 
+#ifndef TIGHT_SQUEEZE
 static void
 printf(const char *fmt,...)
 {
@@ -599,6 +604,7 @@ printf(const char *fmt,...)
     va_end(ap);
     return;
 }
+#endif
 
 static void
 putchar(int c)
