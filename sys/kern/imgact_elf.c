59936dae3d4da77c7e190814f01d12faf24f17ef
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2017-01-08 01:26:54 +0000

    Capture run-time linker information in execve(2).

--- sys/kern/imgact_elf.c
+++ sys/kern/imgact_elf.c
@@ -659,7 +659,8 @@ __elfN(load_file)(struct proc *p, const char *file, u_long *addr,
 	imgp->object = NULL;
 	imgp->execlabel = NULL;
 
-	NDINIT(nd, LOOKUP, LOCKLEAF | FOLLOW, UIO_SYSSPACE, file, curthread);
+	NDINIT(nd, LOOKUP, LOCKLEAF | FOLLOW | AUDITVNODE2, UIO_SYSSPACE,
+	    file, curthread);
 	if ((error = namei(nd)) != 0) {
 		nd->ni_vp = NULL;
 		goto fail;
