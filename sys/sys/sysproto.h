b85603c5cdbc11b2856aafe990fdb74b40f7098e
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2017-03-29 20:36:58 +0000

    Add system-call audit-event mapping for lio_listio(2).

--- sys/sys/sysproto.h
+++ sys/sys/sysproto.h
@@ -2879,7 +2879,7 @@ int	freebsd10_pipe(struct thread *, struct freebsd10_pipe_args *);
 #define	SYS_AUE_aio_error	AUE_AIO_ERROR
 #define	SYS_AUE_freebsd6_aio_read	AUE_AIO_READ
 #define	SYS_AUE_freebsd6_aio_write	AUE_AIO_WRITE
-#define	SYS_AUE_freebsd6_lio_listio	AUE_NULL
+#define	SYS_AUE_freebsd6_lio_listio	AUE_LIO_LISTIO
 #define	SYS_AUE_yield	AUE_NULL
 #define	SYS_AUE_mlockall	AUE_MLOCKALL
 #define	SYS_AUE_munlockall	AUE_MUNLOCKALL
