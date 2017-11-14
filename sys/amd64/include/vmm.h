612b476c6b5a7a8a3f2609f4ab799eaf12803d89
Domagoj Stolfa <ds815@cam.ac.uk>
2017-11-14 13:21:06 +0000

    Import the modified vmm.h

--- sys/amd64/include/vmm.h
+++ sys/amd64/include/vmm.h
@@ -117,6 +117,7 @@ enum x2apic_state {
 #ifdef _KERNEL
 
 #define	VM_MAX_NAMELEN	32
+#define	HV_MAX_NAMELEN	32
 
 struct vm;
 struct vm_exception;
@@ -181,6 +182,12 @@ struct vmm_ops {
 
 extern struct vmm_ops vmm_ops_intel;
 extern struct vmm_ops vmm_ops_amd;
+
+#define BHYVE_MODE		0
+#define VMM_MAX_MODES		1
+
+extern	int	hypervisor_mode;
+extern	int	hypercalls_enabled;
 
 int vm_create(const char *name, struct vm **retvm);
 void vm_destroy(struct vm *vm);
@@ -440,7 +447,7 @@ enum vm_intr_trigger {
 	EDGE_TRIGGER,
 	LEVEL_TRIGGER
 };
-	
+
 /*
  * The 'access' field has the format specified in Table 21-2 of the Intel
  * Architecture Manual vol 3b.
@@ -557,6 +564,7 @@ enum vm_exitcode {
 	VM_EXITCODE_SVM,
 	VM_EXITCODE_REQIDLE,
 	VM_EXITCODE_DEBUG,
+	VM_EXITCODE_HYPERCALL,
 	VM_EXITCODE_MAX
 };
 
@@ -595,6 +603,10 @@ struct vm_task_switch {
 	int		errcode_valid;	/* push 'errcode' on the new stack */
 	enum task_switch_reason reason;
 	struct vm_guest_paging paging;
+};
+
+struct vm_hypercall {
+	struct vm_guest_paging	paging;
 };
 
 struct vm_exit {
@@ -661,7 +673,8 @@ struct vm_exit {
 		struct {
 			enum vm_suspend_how how;
 		} suspended;
-		struct vm_task_switch task_switch;
+		struct vm_task_switch	task_switch;
+		struct vm_hypercall	hypercall;
 	} u;
 };
 
@@ -694,6 +707,8 @@ vm_inject_ss(void *vm, int vcpuid, int errcode)
 }
 
 void vm_inject_pf(void *vm, int vcpuid, int error_code, uint64_t cr2);
+void vm_dtrace_init_install(void *vm, int vcpuid);
+void vm_dtrace_init_uninstall(void *vm, int vcpuid);
 
 int vm_restart_instruction(void *vm, int vcpuid);
 
