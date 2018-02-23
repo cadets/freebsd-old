49f62620379b822a61607d7f9041d3442c5c5c20
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-02-23 19:35:53 -0330

    Refactor analysis code out into dt_analysis.{h,c}.
    This code is quite independent of things in dt_program.{h,c}, so
    refactor it out into its own header and source files. This also gives us
    the opportunity to put this (entirely-CADETS-produced) work under a BSD
    license and to keep our changes to contrib sources small.

---
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_analysis.h
@@ -0,0 +1,82 @@
+/*-
+ * Copyright (c) 2018 Jonathan Anderson
+ * Copyright (c) 2018 Brian Kidney
+ * All rights reserved.
+ *
+ * This software was developed by BAE Systems, the University of Cambridge
+ * Computer Laboratory, and Memorial University under DARPA/AFRL contract
+ * FA8650-15-C-7558 ("CADETS"), as part of the DARPA Transparent Computing
+ * (TC) research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef	_DT_ANALYSIS_H
+#define	_DT_ANALYSIS_H
+
+#include <dtrace.h>
+
+/*
+ * DTrace actions can reference or modify variables (global, thread-local or
+ * clause-local), external memory or internal state (in a dtrace_state_t).
+ */
+
+#define	DTRACE_MODREF_GLOBAL_MOD	0x01
+#define	DTRACE_MODREF_GLOBAL_REF	0x02
+#define	DTRACE_MODREF_THREAD_LOCAL_MOD	0x04
+#define	DTRACE_MODREF_THREAD_LOCAL_REF	0x08
+#define	DTRACE_MODREF_CLAUSE_LOCAL_MOD	0x10
+#define	DTRACE_MODREF_CLAUSE_LOCAL_REF	0x20
+#define	DTRACE_MODREF_MEMORY_MOD	0x40
+#define	DTRACE_MODREF_MEMORY_REF	0x80
+#define	DTRACE_MODREF_STATE_MOD		0x100
+#define	DTRACE_MODREF_STATE_REF		0x200
+
+#define	DTRACE_MODREF_ALL ( \
+	DTRACE_MODREF_GLOBAL_MOD | DTRACE_MODREF_GLOBAL_REF \
+	| DTRACE_MODREF_THREAD_LOCAL_MOD | DTRACE_MODREF_THREAD_LOCAL_REF \
+	| DTRACE_MODREF_CLAUSE_LOCAL_MOD | DTRACE_MODREF_CLAUSE_LOCAL_REF \
+	| DTRACE_MODREF_MEMORY_MOD | DTRACE_MODREF_MEMORY_REF \
+	| DTRACE_MODREF_STATE_MOD | DTRACE_MODREF_STATE_REF \
+	)
+
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/* Analyze the mod/ref behaviour of an action */
+int	dtrace_modref_action(const dtrace_actdesc_t *);
+
+/* Check for conformance with mod/ref policies */
+bool	dtrace_modref_check(int action_modref, int cumulative_modref,
+	const dtrace_probedesc_t *, FILE *output);
+
+/* Analyze the mod/ref behaviour of a DTrace Instruction Formation Object */
+int	dtrace_modref_difo(const dtrace_difo_t *);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* _DT_ANALYSIS_H */
+
