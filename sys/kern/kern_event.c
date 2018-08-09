274f95aa66f4229d61737ec7f0fa9b5c0541f61a
Nathaniel Filardo <nwf20@cl.cam.ac.uk>
2018-08-09 19:39:23 +0100

    [UPSTREAM] kqueue: add EV_KEEPUDATA flag
    This allows us to, for example, NOTE_TRIGGER or EV_{EN,DIS}ABLE events
    without overwriting the stashed pointer.

--- sys/kern/kern_event.c
+++ sys/kern/kern_event.c
@@ -1400,6 +1400,13 @@ kqueue_register(struct kqueue *kq, struct kevent *kev, struct thread *td,
 		return EINVAL;
 
 	if (kev->flags & EV_ADD) {
+		/* Reject an invalid flag pair early */
+		if (kev->flags & EV_KEEPUDATA) {
+			tkn = NULL;
+			error = EINVAL;
+			goto done;
+		}
+
 		/*
 		 * Prevent waiting with locks.  Non-sleepable
 		 * allocation failures are handled in the loop, only
@@ -1588,7 +1595,8 @@ findkn:
 	kn_enter_flux(kn);
 	KQ_UNLOCK(kq);
 	knl = kn_list_lock(kn);
-	kn->kn_kevent.udata = kev->udata;
+	if ((kev->flags & EV_KEEPUDATA) == 0)
+		kn->kn_kevent.udata = kev->udata;
 	if (!fops->f_isfd && fops->f_touch != NULL) {
 		fops->f_touch(kn, kev, EVENT_REGISTER);
 	} else {
