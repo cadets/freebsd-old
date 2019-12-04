72a5da813a03dfcc8a191980a0e71324565a4b49
mm2260 <mm2260@cam.ac.uk>
2019-12-04 11:04:38 +0000

    fix page fault

--- sys/dev/virtio/dtrace/virtio_dtrace.c
+++ sys/dev/virtio/dtrace/virtio_dtrace.c
@@ -638,9 +638,7 @@ vtdtr_ctrl_process_event(struct vtdtr_softc *sc,
 	struct vtdtr_ctrl_pbevent *pb;
 	struct vtdtr_ctrl_provevent *pv;
 	struct vtdtr_ctrl_scriptevent *script_ev;
-
-	script =
-	  
+	
 	device_t dev;
 	int retval;
 	int error;
