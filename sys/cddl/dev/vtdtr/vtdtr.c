84802166df4e8d36ad295f4e281d3f6460e5a913
mm2260 <mm2260@cam.ac.uk>
2019-12-07 12:52:07 +0000

    try build vtdtr in the kernel by adding it to the GENERIC configuration

--- sys/cddl/dev/vtdtr/vtdtr.c
+++ sys/cddl/dev/vtdtr/vtdtr.c
@@ -232,6 +232,8 @@ vtdtr_enqueue(struct vtdtr_event *e)
 		mtx_unlock(&q->mtx);
 	}
 	mtx_unlock(&qtree_mtx);
+
+	// TODO(Mara): Remove this
 	DPRINTF(("Successfully enqueued %s\n",e->args.d_script.script));
 }
 
