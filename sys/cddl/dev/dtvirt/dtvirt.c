70b684b82fa854ead3ac1746e740166d0dcb1595
Domagoj Stolfa <ds815@cam.ac.uk>
2020-02-04 15:10:28 +0000

    Add dtvirt.c and dtvirt.h to the new merge.

---
+++ sys/cddl/dev/dtvirt/dtvirt.c
@@ -0,0 +1,126 @@
+/*-
+ * Copyright (c) 2018 Domagoj Stolfa
+ * All rights reserved.
+ *
+ * This software was developed by BAE Systems, the University of Cambridge
+ * Computer Laboratory, and Memorial University under DARPA/AFRL contract
+ * FA8650-15-C-7558 ("CADETS"), as part of the DARPA Transparent Computing
+ * (TC) research program.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * dtvirt.c is a simple proxy between DTrace and vmm due to linking problems.
+ * It exposes a number of interfaces via function pointers to access internal
+ * vmm state from the DTrace probe context.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/kernel.h>
+#include <sys/module.h>
+#include <sys/dtrace.h>
+#include <machine/vmm.h>
+
+#include "dtvirt.h"
+
+static MALLOC_DEFINE(M_DTVIRT, "dtvirt", "");
+static lwpid_t dtvirt_priv_gettid(void *);
+static uint16_t dtvirt_priv_getns(void *);
+static const char *dtvirt_priv_getname(void *);
+
+lwpid_t (*vmm_gettid)(void *biscuit);
+uint16_t (*vmm_getid)(void *biscuit);
+const char *(*vmm_getname)(void *biscuit);
+
+void
+dtvirt_probe(void *biscuit, int probeid, struct dtvirt_args *dtv_args)
+{
+
+	dtrace_ns_probe(biscuit, probeid, dtv_args);
+}
+
+static int
+dtvirt_handler(module_t mod __unused, int what, void *arg __unused)
+{
+	switch (what) {
+	case MOD_LOAD:
+		dtvirt_gettid = dtvirt_priv_gettid;
+		dtvirt_getns = dtvirt_priv_getns;
+		dtvirt_getname = dtvirt_priv_getname;
+		break;
+	case MOD_UNLOAD:
+		dtvirt_gettid = NULL;
+		dtvirt_getns = NULL;
+		dtvirt_getname = NULL;
+		break;
+	default:
+		break;
+	}
+	return (0);
+}
+
+/*
+ * Get the thread ID of the VM.
+ */
+static lwpid_t
+dtvirt_priv_gettid(void *biscuit)
+{
+
+	if (vmm_gettid != NULL)
+		return (vmm_gettid(biscuit));
+	return (0);
+}
+
+/*
+ * Get a unique identifier of each vm (uint16_t). This is used to scope
+ * thread-local storage in the DTrace probe context.
+ */
+static uint16_t
+dtvirt_priv_getns(void *biscuit)
+{
+
+	if (vmm_getid != NULL)
+		return (vmm_getid(biscuit));
+	return (0);
+}
+
+static const char *
+dtvirt_priv_getname(void *biscuit)
+{
+
+	return (vmm_getname == NULL ? 0 : vmm_getname(biscuit));
+}
+
+static moduledata_t dtvirt_kmod = {
+	"dtvirt",
+	dtvirt_handler,
+	NULL
+};
+
+DECLARE_MODULE(dtvirt, dtvirt_kmod, SI_SUB_DTRACE + 1, SI_ORDER_ANY);
+MODULE_VERSION(dtvirt, 1);
+MODULE_DEPEND(dtvirt, dtrace, 1, 1, 1);
---
+++ sys/cddl/dev/dtvirt/dtvirt.h
@@ -0,0 +1,46 @@
+#ifndef _DTVIRT_H_
+#define _DTVIRT_H_
+
+#include <sys/types.h>
+#include <sys/proc.h>
+
+#define	DTVIRT_ARGS_MAX	10
+
+/*
+ * The arguments that we pass in from the guest to host to implement builtin
+ * variables. (v) denotes that we have to validate the argument before we send
+ * it off to DTrace. (h) denotes variables that can be set by the hypervisor
+ * rather than the guest. However, for simplicity's sake they are all set in the
+ * guest currently.
+ *
+ * XXX(dstolfa): EXECNAME and EXECARGS are currently guest-controlled in terms
+ * of what the length is. This requires verification code on the interface and
+ * is probably not something that we should try exercise. Would it make sense to
+ * make this static to avoid any kind of validation necessary and forcefully
+ * NULL-terminate a string?
+ */
+struct dtvirt_args {
+	uintptr_t dtv_args[DTVIRT_ARGS_MAX]; /* guest probe args */
+	void *dtv_curthread;		     /* guest thread */
+	char *dtv_execname;                  /* (v) guest execname */
+	char *dtv_execargs;		     /* (v) guest execargs */
+	lwpid_t dtv_tid;		     /* guest tid */
+	pid_t dtv_pid;			     /* guest pid */
+	pid_t dtv_ppid;			     /* guest ppid */
+	uid_t dtv_uid;			     /* guest uid */
+	gid_t dtv_gid;			     /* guest gid */
+	int dtv_errno;			     /* guest errno */
+	u_int dtv_curcpu;		     /* (h) guest curcpu */
+	u_int dtv_execargs_len;
+	char *dtv_probeprov;
+	char *dtv_probemod;
+	char *dtv_probefunc;
+	char *dtv_probename;
+};
+
+extern void dtvirt_probe(void *, int, struct dtvirt_args *);
+extern lwpid_t (*dtvirt_gettid)(void *);
+extern uint16_t (*dtvirt_getns)(void *);
+extern const char *(*dtvirt_getname)(void *);
+
+#endif
