b14207898dd0ba2ba498abb5ebec4a27803a1c8d
mm2260 <mm2260@cam.ac.uk>
2020-02-26 14:29:24 +0000

    revert and see if pthread problem is still there

--- cddl/contrib/opensolaris/cmd/dtrace/dtrace.c
+++ cddl/contrib/opensolaris/cmd/dtrace/dtrace.c
@@ -73,7 +73,7 @@ typedef struct dtrace_cmd
 
 struct dtrace_guest_entry
 {
-	dtrace_bufdesc_t desc;
+	dtrace_bufdesc_t *desc;
 	STAILQ_ENTRY(dtrace_guest_entry)
 	entries;
 };
@@ -1551,7 +1551,7 @@ filter_machines(char *filt)
 static void dtrace_gtq_enqueue(struct dtrace_guestq *gtq, struct dtrace_guest_entry *trc_entry)
 {
 	STAILQ_INSERT_TAIL(&gtq->head, trc_entry, entries);
-	printf("Enqueued trace element. \n");
+	printf(" Enqueued trace element. \n");
 }
 
 struct dtrace_guest_entry *dtrace_gtq_dequeue(struct dtrace_guestq *gtq)
@@ -1562,7 +1562,7 @@ struct dtrace_guest_entry *dtrace_gtq_dequeue(struct dtrace_guestq *gtq)
 	{
 		STAILQ_REMOVE_HEAD(&gtq->head, entries);
 	}
-	printf("Dequeued trace element of size: %d",trc_entry->desc.dtbd_size);
+	printf("Dequeued trace element of size: %d",trc_entry->desc->dtbd_size);
 
 	return (trc_entry);
 }
@@ -1665,9 +1665,11 @@ static void *read_trace_data(void *xgtq)
 	printf("About to process trace data. \n");
 	for (;;)
 	{
-		dtrace_bufdesc_t buf;
+		dtrace_bufdesc_t *buf;
 		struct dtrace_guest_entry *trc_entry;
 
+		buf = malloc(sizeof(dtrace_bufdesc_t));
+		assert(buf != NULL);
 		trc_entry = malloc(sizeof(struct dtrace_guest_entry));
 		assert(trc_entry != NULL);
 
@@ -1686,33 +1688,31 @@ static void *read_trace_data(void *xgtq)
 		printf("open() were called.\n");
 
 		printf("About to read trace data. \n");
-		sz = read(fd, &buf.dtbd_size, sizeof(uint64_t));
+		sz = read(fd, &buf->dtbd_size, sizeof(uint64_t));
 		assert(sz > 0);
-		printf("Size: %d\n", buf.dtbd_size);
-		sz = read(fd, &buf.dtbd_cpu, sizeof(uint32_t));
+		printf("Size: %d\n", buf->dtbd_size);
+		sz = read(fd, &buf->dtbd_cpu, sizeof(uint32_t));
 		assert(sz > 0);
-		printf("Cpu: %d\n", buf.dtbd_errors);
-		sz = read(fd, &buf.dtbd_errors, sizeof(uint32_t));
+		printf("Cpu: %d\n", buf->dtbd_errors);
+		sz = read(fd, &buf->dtbd_errors, sizeof(uint32_t));
 		assert(sz > 0);
-		printf("Errors: %d\n", buf.dtbd_errors);
-		sz = read(fd, &buf.dtbd_drops, sizeof(uint64_t));
+		printf("Errors: %d\n", buf->dtbd_errors);
+		sz = read(fd, &buf->dtbd_drops, sizeof(uint64_t));
 		assert(sz > 0);
-		printf("Drops: %d\n", buf.dtbd_drops);
-		sz = read(fd, &buf.dtbd_oldest, sizeof(uint64_t));
+		printf("Drops: %d\n", buf->dtbd_drops);
+		sz = read(fd, &buf->dtbd_oldest, sizeof(uint64_t));
 		assert(sz > 0);
-		printf("Oldest: %d\n", buf.dtbd_oldest);
-		sz = read(fd, &buf.dtbd_timestamp, sizeof(uint64_t));
+		printf("Oldest: %d\n", buf->dtbd_oldest);
+		sz = read(fd, &buf->dtbd_timestamp, sizeof(uint64_t));
 		assert(sz > 0);
-		printf("Timestamp: %d\n", buf.dtbd_timestamp);
-		buf.dtbd_data = malloc(buf.dtbd_size);
-		sz = read(fd, buf.dtbd_data, buf.dtbd_size);
-		assert(sz == buf.dtbd_size);
-		fflush(stdout);
-
+		printf("Timestamp: %d\n", buf->dtbd_timestamp);
+		buf->dtbd_data = malloc(buf->dtbd_size);
+		sz = read(fd, buf->dtbd_data, buf->dtbd_size);
+		assert(sz == buf->dtbd_size);
 		trc_entry->desc = buf;
 		dtrace_gtq_enqueue(gtq, trc_entry);
 		printf("Successfully enqueued trace element");
-		fflush(stdout);
+	
 
 		// TODO: discover what happens if you actually print this
 		// since assertion doesn't fail we should be okay
@@ -1879,7 +1879,7 @@ int main(int argc, char *argv[])
 		if (optind < argc)
 			g_argv[g_argc++] = argv[optind];
 	}
-	
+
 	/*
 	 * We are tracing a guest and assume that we've done everything up to
 	 * dtrace_work which prints trace data
@@ -1887,20 +1887,14 @@ int main(int argc, char *argv[])
 	if (h_mode == 1)
 	{	
 		struct dtrace_guestq *gtq;
-		pthread_t *trace_reader;
+		pthread_t trace_reader;
 		const char *file_path;
 		file_path = argv[argc - 1];
-		write_script(file_path);
-		fflush(stdout);
 		gtq = malloc(sizeof(struct dtrace_guestq));
-		assert(gtq != NULL);
+		write_script(file_path);
 		STAILQ_INIT(&gtq->head);
 		printf("Guest queue successfully initialised");
-		fflush(stdout);
-		printf("Creating thread to read trace data");
-		trace_reader = pthread_create(trace_reader, NULL, read_trace_data, gtq);
-		printf("about to process trace data");
-		fflush(stdout);
+		trace_reader = pthread_create(&trace_reader, NULL, read_trace_data,gtq);
 		process_trace_data();
 		// no need to close dtrace since we don't even open it here
 		return (g_status);
