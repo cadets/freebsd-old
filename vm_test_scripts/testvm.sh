b08f00b86e703f277140e12dbd691ba2551a5466
mm2260 <mm2260@cam.ac.uk>
2020-03-19 23:27:59 +0200

    try to create two VMs

--- vm_test_scripts/testvm.sh
+++ vm_test_scripts/testvm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-bhyvectl --destroy --vm=test
+bhyvectl --destroy --vm=$1
 
-bhyveload -d test.img -m 1g test
-bhyve -A -H -P -s 0:0,hostbridge -s 1:0,lpc -s 2:0,virtio-blk,./test.img -s 3:0,virtio-dtrace -l com1,stdio -c 2 -m 1g -t test
+bhyveload -d $1.img -m 1g $1
+bhyve -A -H -P -s 0:0,hostbridge -s 1:0,lpc -s 2:0,virtio-blk,./$1.img -s 3:0,virtio-dtrace -l com1,stdio -c 2 -m 1g -t $1
