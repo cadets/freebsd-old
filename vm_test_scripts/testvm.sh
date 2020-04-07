7d7b5d0f452b18631db5ad9c6ee7769d7b9c1093
mm2260 <mm2260@cam.ac.uk>
2020-04-07 11:35:00 +0300

    spawn VMs with 10GB RAM

--- vm_test_scripts/testvm.sh
+++ vm_test_scripts/testvm.sh
@@ -2,5 +2,5 @@
 
 bhyvectl --destroy --vm=test
 
-bhyveload -d test.img -m 30g test
-bhyve -A -H -P -s 0:0,hostbridge -s 1:0,lpc -s 2:0,virtio-blk,./test.img -s 3:0,virtio-dtrace -l com1,stdio -c 6 -m 30g -t test
+bhyveload -d test.img -m 10g test
+bhyve -A -H -P -s 0:0,hostbridge -s 1:0,lpc -s 2:0,virtio-blk,./test.img -s 3:0,virtio-dtrace -l com1,stdio -c 6 -m 10g -t test
