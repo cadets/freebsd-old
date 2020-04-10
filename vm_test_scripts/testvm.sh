de7b08269b55c352a42e9b321d97deec601992f4
mm2260 <mm2260@cam.ac.uk>
2020-04-10 15:01:33 +0300

    add virtio-net

--- vm_test_scripts/testvm.sh
+++ vm_test_scripts/testvm.sh
@@ -3,4 +3,4 @@
 bhyvectl --destroy --vm=test
 
 bhyveload -d test.img -m 10g test
-bhyve -A -H -P -s 0:0,hostbridge -s 1:0,lpc -s 2:0,virtio-blk,./test.img -s 3:0,virtio-dtrace -l com1,stdio -c 6 -m 10g -t test
+bhyve -A -H -P -s 0:0,hostbridge -s 1:0,lpc -s 2:0,virtio-net,tap0 -s 3:0,virtio-blk,./test.img -s 4:0,virtio-dtrace -l com1,stdio -c 6 -m 10g -t test
