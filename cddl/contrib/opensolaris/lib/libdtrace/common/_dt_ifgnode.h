ba3ac993d939a754893536e1e5e90ccc31290e65
Domagoj Stolfa <ds815@gmx.com>
2021-06-02 20:00:20 +0100

    dt_ifgnode: formatting.

--- cddl/contrib/opensolaris/lib/libdtrace/common/_dt_ifgnode.h
+++ cddl/contrib/opensolaris/lib/libdtrace/common/_dt_ifgnode.h
@@ -35,16 +35,16 @@
 #include <dt_list.h>
 
 typedef struct dt_node_kind {
-	int				dtnk_kind;	/* kind (see below) */
-#define DT_NKIND_REG	1				/* register */
-#define DT_NKIND_VAR	2				/* variable */
-#define DT_NKIND_STACK	3				/* stack */
+	int                             dtnk_kind; /* kind (see below) */
+#define DT_NKIND_REG	1                          /* register */
+#define DT_NKIND_VAR	2                          /* variable */
+#define DT_NKIND_STACK	3                          /* stack */
 	union {
-		uint8_t			rd;		/* DT_RKIND_REG */
-		struct {				/* DT_RKIND_VAR */
-			uint16_t	var;
-			uint8_t		scope;
-			uint8_t		varkind;
+		uint8_t                 rd;        /* DT_RKIND_REG */
+		struct {                           /* DT_RKIND_VAR */
+			uint16_t        var;
+			uint8_t         scope;
+			uint8_t         varkind;
 		} v;
 	} u;
 #define dtnk_rd		u.rd
@@ -54,39 +54,40 @@ typedef struct dt_node_kind {
 } dt_node_kind_t;
 
 typedef struct dt_ifg_node {
-	size_t		din_uidx;	/* index of the use site */
-	dt_list_t	din_r1defs;	/* type flow list for r1 */
-	dt_list_t	din_r2defs;	/* type flow list for r2 */
-	dt_list_t	din_r1datadefs;	/* data flow list for r1 */
-	dt_list_t	din_r2datadefs;	/* data flow list for r2 */
-	dt_list_t	din_vardefs;	/* list of variable defns in DIFO */
-	int		din_type;	/* D type */
-	ctf_id_t	din_ctfid;	/* CTF type */
-	char		*din_sym;	/* symbol (if applicable) */
-	dtrace_difo_t	*din_difo;	/* DIFO which this node belongs to */
+	size_t          din_uidx;       /* index of the use site */
+	dt_list_t       din_r1defs;     /* type flow list for r1 */
+	dt_list_t       din_r2defs;     /* type flow list for r2 */
+	dt_list_t       din_r1datadefs; /* data flow list for r1 */
+	dt_list_t       din_r2datadefs; /* data flow list for r2 */
+	dt_list_t       din_vardefs;    /* list of variable defns in DIFO */
+	int             din_type;       /* D type */
+	dt_typefile_t   *din_tf;        /* type file */ 
+	ctf_id_t        din_ctfid;      /* CTF type */
+	char            *din_sym;       /* symbol (if applicable) */
+	dtrace_difo_t   *din_difo;      /* DIFO which this node belongs to */
 #define din_buf din_difo->dtdo_buf
-	ctf_membinfo_t	*din_mip;	/* CTF member info (type, offs) */
-	dt_list_t	din_stacklist;	/* list of pushtr/pushtv nodes */
-        dt_node_kind_t	din_kind;	/* node kind (reg, var, stack) */
-	dt_list_t	din_usetxs;	/* usetx insn list defining the node */
-	int		din_relocated;	/* relocated or not? */
+	ctf_membinfo_t  *din_mip;       /* CTF member info (type, offs) */
+	dt_list_t       din_stacklist;  /* list of pushtr/pushtv nodes */
+        dt_node_kind_t  din_kind;       /* node kind (reg, var, stack) */
+	dt_list_t       din_usetxs;     /* usetx insn list defining the node */
+	int             din_relocated;  /* relocated or not? */
 } dt_ifg_node_t;
 
 typedef struct dt_ifg_list {
-	dt_list_t	dil_list;
-	dt_ifg_node_t	*dil_ifgnode;
+	dt_list_t       dil_list;
+	dt_ifg_node_t   *dil_ifgnode;
 } dt_ifg_list_t;
 
 typedef struct dt_stacklist {
-	dt_list_t	dsl_list;	/* next/prev for dt_stacklist_t */
-	dt_list_t	dsl_stack;	/* the stack itself */
-	dt_list_t	dsl_identifier;	/* identifies this list
-					   (basic block id list) */
+	dt_list_t       dsl_list;       /* next/prev for dt_stacklist_t */
+	dt_list_t       dsl_stack;      /* the stack itself */
+	dt_list_t       dsl_identifier; /* identifies this list
+	                                   (basic block id list) */
 } dt_stacklist_t;
 
 typedef struct dt_stack {
-	dt_list_t	ds_list;
-	dt_ifg_node_t	*ds_ifgnode;
+	dt_list_t       ds_list;
+	dt_ifg_node_t   *ds_ifgnode;
 } dt_stack_t;
 
 #endif /* __DT_RELO_H_ */
