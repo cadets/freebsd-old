e937b7c56f951feac3325104ad77c0f2183ca3a8
Domagoj Stolfa <ds815@gmx.com>
2021-06-02 17:57:57 +0100

    libdtrace: add dt_typefile for CTF containers.
    Currently, we use a single CTF container to do type inference on DIF via
    a global ctf_file. This commit is the start of a series of diffs that
    implements a 'dynamic' type-checker that depends on which modules are
    loaded rather than simply checking only the kernel.

---
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_typefile.c
@@ -0,0 +1,116 @@
+/*-
+ * Copyright (c) 2021 Domagoj Stolfa
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+int
+dt_open_typefiles(void)
+{
+	int err = 0;
+	int kld;
+	struct kld_file_stat kldinfo;
+	dt_typefile_t *typef;
+
+	for (kld = kldnext(0); kld > 0; kld = kldnext(kld)) {
+		kldinfo.version = sizeof(kldinfo);
+		if (kldstat(kld, &kldinfo) < 0)
+			return (-1);
+
+		/*
+		 * We open the type file and save it in a list that we will use
+		 * later in type-checking and applying relocations.
+		 */
+		typef = malloc(sizeof(dt_typefile_t));
+		if (typef == NULL)
+			errx(EXIT_FAILURE, "malloc() failed with: %s\n",
+			    strerror(errno));
+
+		memcpy(typef->pathname, kldinfo.pathname, MAXPATHLEN);
+		memcpy(typef->modname, kldinfo.name, MAXPATHLEN);
+		typef->ctf_file = ctf_open(typef->pathname, &err);
+		if (err != 0)
+			errx(EXIT_FAILURE, "failed opening %s: %s\n",
+			    typef->pathname,
+			    ctf_errmsg(ctf_errno(typef->ctf_file)));
+		dt_list_append(&typefiles, typef);
+	}
+
+	return (0);
+}
+
+void
+dt_cleanup_typefiles(void)
+{
+	dt_typefile_t *typef;
+
+	while ((typef = dt_list_next(&typefiles)) != NULL) {
+		dt_list_delete(&typefiles, typef);
+		free(typef);
+	}
+}
+
+dt_typefile_t *
+dt_get_typefile_by_kmod(const char *kmod)
+{
+	dt_typefile_t *typef;
+
+	for (typef = dt_list_next(&typefiles); typef;
+	     typef = dt_list_next(typef)) {
+		if (strcmp(typef->modname, kmod) == 0)
+			return (typef);
+	}
+
+	return (NULL);
+}
+
+dt_typefile_t *
+dt_get_typefile_by_ctf(ctf_file_t *ctf)
+{
+	dt_typefile_t *typef;
+
+	for (typef = dt_list_next(&typefiles); typef;
+	     typef = dt_list_next(typef)) {
+		if ((typef->ctf_file == ctf) == 0)
+			return (typef);
+	}
+
+	return (NULL);
+}
+
+ctf_id_t
+dt_get_ctfid_by_kmod(const char *kmod, const char *type)
+{
+	dt_typefile_t *typef;
+	typef = dt_get_typefile_by_kmod(kmod);
+
+	return (ctf_lookup_by_name(typef->ctf_file, type))
+}
+
+const char *
+dt_typefile_error(dt_typefile_t *typef)
+{
+
+	return (ctf_errmsg(ctf_errno(typef->ctf_file)));
+}
---
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_typefile.h
@@ -0,0 +1,42 @@
+/*-
+ * Copyright (c) 2021 Domagoj Stolfa
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef __DT_TYPEFILE_T_
+#define __DT_TYPEFILE_T_
+
+#include <_dt_typefile.h>
+
+int dt_open_typefiles(void);
+void dt_cleanup_typefiles(void);
+
+dt_typefile_t *dt_get_typefile_by_kmod(const char *);
+dt_typefile_t *dt_get_typefile_by_ctf(ctf_file_t *);
+ctf_id_t dt_get_ctfid_by_kmod(const char *);
+
+const char *dt_typefile_error(dt_typefile_t *);
+
+#endif
