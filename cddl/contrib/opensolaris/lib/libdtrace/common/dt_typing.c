3a44a57467d8f6045cfccde8cfa3d51fd4165c3d
Domagoj Stolfa <ds815@gmx.com>
2021-06-03 23:52:01 +0100

    dt_typing: DIF_SUBR_RAND now uses typefiles.

--- cddl/contrib/opensolaris/lib/libdtrace/common/dt_typing.c
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_typing.c
@@ -2200,10 +2200,11 @@ dt_infer_type(dt_ifg_node_t *n)
 		 */
 		switch (subr) {
 		case DIF_SUBR_RAND:
-			n->din_ctfid = ctf_lookup_by_name(ctf_file, "uint64_t");
+			n->din_ctfid = dt_typefile_ctfid(n->din_tf, "uint64_t");
 			if (n->din_ctfid == CTF_ERR)
-				dt_set_progerr(g_dtp, g_pgp, "failed to get type uint64_t: %s",
-				    ctf_errmsg(ctf_errno(ctf_file)));
+				dt_set_progerr(g_dtp, g_pgp,
+				    "failed to get type uint64_t: %s",
+				    dt_typefile_error(n->din_tf));
 
 			n->din_type = DIF_TYPE_CTF;
 			break;
