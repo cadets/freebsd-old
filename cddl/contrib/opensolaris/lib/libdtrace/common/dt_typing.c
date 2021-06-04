3cc1b0bfba77ec4e64918680987c42b720582aef
Domagoj Stolfa <ds815@gmx.com>
2021-06-04 12:35:50 +0100

    dt_typing: DIF_SUBR_MSG(D)SIZE now use typefiles.

--- cddl/contrib/opensolaris/lib/libdtrace/common/dt_typing.c
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_typing.c
@@ -3007,12 +3007,14 @@ dt_infer_type(dt_ifg_node_t *n)
 				    "msg(d)size() first argument is NULL");
 
 			arg0 = se->ds_ifgnode;
+			assert(arg0->din_tf != NULL);
 
-			if (ctf_type_name(ctf_file,
+			if (dt_typefile_typename(arg0->din_tf,
 			    arg0->din_ctfid, buf, sizeof(buf)) != (char *)buf)
-				dt_set_progerr(g_dtp, g_pgp, "failed at getting type name"
-				    " %ld: %s", arg0->din_ctfid,
-				    ctf_errmsg(ctf_errno(ctf_file)));
+				dt_set_progerr(g_dtp, g_pgp,
+				    "failed at getting type name %ld: %s",
+				    arg0->din_ctfid,
+				    dt_typefile_error(arg0->din_tf));
 
 			/*
 			 * If the argument type is wrong, fail to type check.
@@ -3023,11 +3025,11 @@ dt_infer_type(dt_ifg_node_t *n)
 				return (-1);
 			}
 
-			n->din_ctfid = ctf_lookup_by_name(ctf_file, "size_t");
+			n->din_ctfid = dt_typefile_ctfid(n->din_tf, "size_t");
 			if (n->din_ctfid == CTF_ERR)
-				errx(EXIT_FAILURE,
+				dt_set_progerr(g_dtp, g_pgp,
 				    "failed to get type size_t: %s",
-				    ctf_errmsg(ctf_errno(ctf_file)));
+				    dt_typefile_error(n->din_tf));
 
 			n->din_type = DIF_TYPE_CTF;
 			break;
