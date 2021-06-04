c18c86600e57cdec1e84b3dd11fe1770fd81b6a7
Domagoj Stolfa <ds815@gmx.com>
2021-06-04 13:30:27 +0100

    dt_typing: DT_SUBR_MEMREF now uses typefiles.

--- cddl/contrib/opensolaris/lib/libdtrace/common/dt_typing.c
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_typing.c
@@ -3706,12 +3706,14 @@ dt_infer_type(dt_ifg_node_t *n)
 				    "memref() first argument is NULL");
 
 			arg0 = se->ds_ifgnode;
+			assert(arg0->din_tf != NULL);
 
-			if (ctf_type_name(ctf_file,
+			if (dt_typefile_typename(arg0->din_tf,
 			    arg0->din_ctfid, buf, sizeof(buf)) != (char *)buf)
-				dt_set_progerr(g_dtp, g_pgp, "failed at getting type name"
-				    " %ld: %s", arg0->din_ctfid,
-				    ctf_errmsg(ctf_errno(ctf_file)));
+				dt_set_progerr(g_dtp, g_pgp,
+				    "failed at getting type name %ld: %s",
+				    arg0->din_ctfid,
+				    dt_typefile_error(arg0->din_tf));
 
 			/*
 			 * If the argument type is wrong, fail to type check.
@@ -3727,16 +3729,18 @@ dt_infer_type(dt_ifg_node_t *n)
 			 */
 			se = dt_list_next(se);
 			if (se == NULL || se->ds_ifgnode == NULL)
-				errx(EXIT_FAILURE,
+				dt_set_progerr(g_dtp, g_pgp,
 				    "memref() second argument is NULL");
 
 			arg1 = se->ds_ifgnode;
+			assert(arg1->din_tf != NULL);
 
-			if (ctf_type_name(ctf_file,
+			if (dt_typefile_typename(arg1->din_tf,
 			    arg1->din_ctfid, buf, sizeof(buf)) != (char *)buf)
-				dt_set_progerr(g_dtp, g_pgp, "failed at getting type name"
-				    " %ld: %s", arg1->din_ctfid,
-				    ctf_errmsg(ctf_errno(ctf_file)));
+				dt_set_progerr(g_dtp, g_pgp,
+				    "failed at getting type name %ld: %s",
+				    arg1->din_ctfid,
+				    dt_typefile_error(arg1->din_tf));
 
 			/*
 			 * If the argument type is wrong, fail to type check.
@@ -3747,12 +3751,12 @@ dt_infer_type(dt_ifg_node_t *n)
 				return (-1);
 			}
 
-			n->din_ctfid = ctf_lookup_by_name(
-			    ctf_file, "uintptr_t *");
+			n->din_ctfid =
+			    dt_typefile_ctfid(n->din_tf, "uintptr_t *");
 			if (n->din_ctfid == CTF_ERR)
-				errx(EXIT_FAILURE,
+				dt_set_progerr(g_dtp, g_pgp,
 				    "failed to get type uintptr_t *: %s",
-				    ctf_errmsg(ctf_errno(ctf_file)));
+				    dt_typefile_error(n->din_tf));
 
 			n->din_type = DIF_TYPE_CTF;
 			break;
