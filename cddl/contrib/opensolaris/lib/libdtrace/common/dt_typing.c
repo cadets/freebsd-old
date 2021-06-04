6dfb8749370f9f1ad6b2a305cfbfe7c9795a26f3
Domagoj Stolfa <ds815@gmx.com>
2021-06-04 14:48:59 +0100

    dt_typing: DIF_SUBR_STRTOLL now uses typefiles.

--- cddl/contrib/opensolaris/lib/libdtrace/common/dt_typing.c
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_typing.c
@@ -3933,12 +3933,14 @@ dt_infer_type(dt_ifg_node_t *n)
 				    "strtoll() first argument is NULL");
 
 			arg0 = se->ds_ifgnode;
+			assert(arg0->din_tf != NULL);
 
-			if (ctf_type_name(ctf_file,
+			if (dt_typefile_typename(arg0->din_tf,
 			    arg0->din_ctfid, buf, sizeof(buf)) != (char *)buf)
-				dt_set_progerr(g_dtp, g_pgp, "failed at getting type name"
-				    " %ld: %s", arg0->din_ctfid,
-				    ctf_errmsg(ctf_errno(ctf_file)));
+				dt_set_progerr(g_dtp, g_pgp,
+				    "failed at getting type name %ld: %s",
+				    arg0->din_ctfid,
+				    dt_typefile_error(arg0->din_tf));
 
 			/*
 			 * If the argument type is wrong, fail to type check.
@@ -3959,14 +3961,15 @@ dt_infer_type(dt_ifg_node_t *n)
 					    "strtoll() ds_ifgnode is NULL");
 
 				arg1 = se->ds_ifgnode;
+				assert(arg1->din_tf != NULL);
 
-				if (ctf_type_name(ctf_file,
+				if (dt_typefile_typename(arg1->din_tf,
 				    arg1->din_ctfid,
 				    buf, sizeof(buf)) != (char *)buf)
-					errx(EXIT_FAILURE,
+					dt_set_progerr(g_dtp, g_pgp,
 					    "failed at getting type name"
 					    " %ld: %s", arg1->din_ctfid,
-					    ctf_errmsg(ctf_errno(ctf_file)));
+					    dt_typefile_error(arg1->din_tf));
 
 				/*
 				 * If the argument type is wrong, fail to type check.
@@ -3978,11 +3981,11 @@ dt_infer_type(dt_ifg_node_t *n)
 				}
 			}
 
-			n->din_ctfid = ctf_lookup_by_name(ctf_file, "int64_t");
+			n->din_ctfid = dt_typefile_ctfid(n->din_tf, "int64_t");
 			if (n->din_ctfid == CTF_ERR)
-				errx(EXIT_FAILURE,
+				dt_set_progerr(g_dtp, g_pgp,
 				    "failed to get type int64_t: %s",
-				    ctf_errmsg(ctf_errno(ctf_file)));
+				    dt_typefile_error(n->din_tf));
 
 			n->din_type = DIF_TYPE_CTF;
 			break;
