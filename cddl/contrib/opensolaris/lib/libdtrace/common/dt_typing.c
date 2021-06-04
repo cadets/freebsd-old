c8ba5b73cdb1259231c9fa4e94d434e462ce94d7
Domagoj Stolfa <ds815@gmx.com>
2021-06-04 12:22:36 +0100

    dt_typing: DIF_SUBR_COPYOUTSTR now uses typefiles.

--- cddl/contrib/opensolaris/lib/libdtrace/common/dt_typing.c
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_typing.c
@@ -2711,12 +2711,14 @@ dt_infer_type(dt_ifg_node_t *n)
 				    "copyoutstr() first argument is NULL");
 
 			arg0 = se->ds_ifgnode;
+			assert(arg0->din_tf != NULL);
 
-			if (ctf_type_name(ctf_file,
+			if (dt_typefile_typename(arg0->din_tf,
 			    arg0->din_ctfid, buf, sizeof(buf)) != (char *)buf)
-				dt_set_progerr(g_dtp, g_pgp, "failed at getting type name"
-				    " %ld: %s", arg0->din_ctfid,
-				    ctf_errmsg(ctf_errno(ctf_file)));
+				dt_set_progerr(g_dtp, g_pgp,
+				    "failed at getting type name %ld: %s",
+				    arg0->din_ctfid,
+				    dt_typefile_error(arg0->din_tf));
 
 			/*
 			 * If the argument type is wrong, fail to type check.
@@ -2736,12 +2738,14 @@ dt_infer_type(dt_ifg_node_t *n)
 				    "copyoutstr() second argument is NULL");
 
 			arg1 = se->ds_ifgnode;
+			assert(arg1->din_tf != NULL);
 
-			if (ctf_type_name(ctf_file,
+			if (dt_typefile_typename(arg1->din_tf,
 			    arg1->din_ctfid, buf, sizeof(buf)) != (char *)buf)
-				dt_set_progerr(g_dtp, g_pgp, "failed at getting type name"
-				    " %ld: %s", arg1->din_ctfid,
-				    ctf_errmsg(ctf_errno(ctf_file)));
+				dt_set_progerr(g_dtp, g_pgp,
+				    "failed at getting type name %ld: %s",
+				    arg1->din_ctfid,
+				    dt_typefile_error(arg1->din_tf));
 
 			/*
 			 * If the argument type is wrong, fail to type check.
@@ -2761,12 +2765,14 @@ dt_infer_type(dt_ifg_node_t *n)
 				    "copyoutstr() third argument is NULL");
 
 			arg2 = se->ds_ifgnode;
+			assert(arg2->din_tf != NULL);
 
-			if (ctf_type_name(ctf_file,
+			if (dt_typefile_typename(arg2->din_tf,
 			    arg2->din_ctfid, buf, sizeof(buf)) != (char *)buf)
-				dt_set_progerr(g_dtp, g_pgp, "failed at getting type name"
-				    " %ld: %s", arg2->din_ctfid,
-				    ctf_errmsg(ctf_errno(ctf_file)));
+				dt_set_progerr(g_dtp, g_pgp,
+				    "failed at getting type name %ld: %s",
+				    arg2->din_ctfid,
+				    dt_typefile_error(arg2->din_tf));
 
 			/*
 			 * If the argument type is wrong, fail to type check.
