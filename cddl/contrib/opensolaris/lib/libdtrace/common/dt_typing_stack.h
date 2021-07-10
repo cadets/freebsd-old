4bc87fa62f1307afb3e6ec52a16d5a9b5e225d09
Domagoj Stolfa <ds815@gmx.com>
2021-07-10 22:39:43 +0100

    dtrace (dt_typing): move stack-related typing into its own file.

---
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_typing_stack.h
@@ -0,0 +1,40 @@
+/*-
+ * Copyright (c) 2021 Domagoj Stolfa
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef _DT_TYPING_STACK_H_
+#define _DT_TYPING_STACK_H_
+
+#include <sys/dtrace.h>
+
+#include <_dt_ifgnode.h>
+#include <dt_list.h>
+
+extern int dt_var_stack_typecheck(dt_ifg_node_t *, dt_ifg_node_t *,
+    dtrace_difv_t *);
+extern dt_list_t *dt_typecheck_stack(dt_list_t *, int *);
+
+#endif /* _DT_TYPING_STACK_H_ */
