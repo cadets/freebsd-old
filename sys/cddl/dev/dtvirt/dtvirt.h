41b6290ed68094283c080bc9cef64d861bf7f952
Domagoj Stolfa <ds815@gmx.com>
2021-07-20 14:41:45 +0100

    dtvirt: Remove an out-of-date XXX.

--- sys/cddl/dev/dtvirt/dtvirt.h
+++ sys/cddl/dev/dtvirt/dtvirt.h
@@ -12,12 +12,6 @@
  * it off to DTrace. (h) denotes variables that can be set by the hypervisor
  * rather than the guest. However, for simplicity's sake they are all set in the
  * guest currently.
- *
- * XXX(dstolfa): EXECNAME and EXECARGS are currently guest-controlled in terms
- * of what the length is. This requires verification code on the interface and
- * is probably not something that we should try exercise. Would it make sense to
- * make this static to avoid any kind of validation necessary and forcefully
- * NULL-terminate a string?
  */
 struct dtvirt_args {
 	uintptr_t dtv_args[DTVIRT_ARGS_MAX];	/* guest probe args */
