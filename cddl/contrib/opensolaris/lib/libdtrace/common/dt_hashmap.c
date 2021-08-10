538e3578465a2e72eb0446586965d1bd9a339b7e
Domagoj Stolfa <ds815@gmx.com>
2021-08-10 01:01:32 +0100

    dtrace: Add a new DTrace library interface for hashmaps.

---
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_hashmap.c
@@ -0,0 +1,248 @@
+/*- 
+ * Copyright (c) 2021 Domagoj Stolfa <ds815@gmx.com>
+ *
+ * Copyright (c) 2014 Dag-Erling Smørgrav
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+
+#include <sys/hash.h>
+#include <sys/endian.h>
+#include <sys/stdint.h>
+#include <sys/types.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <dt_hashmap.h>
+
+#define rol32(i32, n) ((i32) << (n) | (i32) >> (32 - (n)))
+
+/*
+ * Simple implementation of the Murmur3-32 hash function.
+ *
+ * This implementation is slow but safe.  It can be made significantly
+ * faster if the caller guarantees that the input is correctly aligned for
+ * 32-bit reads, and slightly faster yet if the caller guarantees that the
+ * length of the input is always a multiple of 4 bytes.
+ */
+static uint32_t
+mm3_hash(const void *data, size_t len, uint32_t seed)
+{
+	const uint8_t *bytes;
+	uint32_t hash, k;
+	size_t res;
+
+	/* initialization */
+	bytes = data;
+	res = len;
+	hash = seed;
+
+	/* main loop */
+	while (res >= 4) {
+		/* replace with le32toh() if input is aligned */
+		k = le32dec(bytes);
+		bytes += 4;
+		res -= 4;
+		k *= 0xcc9e2d51;
+		k = rol32(k, 15);
+		k *= 0x1b873593;
+		hash ^= k;
+		hash = rol32(hash, 13);
+		hash *= 5;
+		hash += 0xe6546b64;
+	}
+
+	/* remainder */
+	/* remove if input length is a multiple of 4 */
+	if (res > 0) {
+		k = 0;
+		switch (res) {
+		case 3:
+			k |= bytes[2] << 16;
+		case 2:
+			k |= bytes[1] << 8;
+		case 1:
+			k |= bytes[0];
+			k *= 0xcc9e2d51;
+			k = rol32(k, 15);
+			k *= 0x1b873593;
+			hash ^= k;
+			break;
+		}
+	}
+
+	/* finalize */
+	hash ^= (uint32_t)len;
+	hash ^= hash >> 16;
+	hash *= 0x85ebca6b;
+	hash ^= hash >> 13;
+	hash *= 0xc2b2ae35;
+	hash ^= hash >> 16;
+	return (hash);
+}
+
+/*
+ * Simplified version of the above optimized for aligned sequences of
+ * 32-bit words.  The count argument is the number of words, not the
+ * length in bytes.
+ */
+static uint32_t
+mm3_hash32(const uint32_t *data, size_t count, uint32_t seed)
+{
+	uint32_t hash, k;
+	size_t res;
+
+	/* iterate */
+	for (res = count, hash = seed; res > 0; res--, data++) {
+		k = le32toh(*data);
+		k *= 0xcc9e2d51;
+		k = rol32(k, 15);
+		k *= 0x1b873593;
+		hash ^= k;
+		hash = rol32(hash, 13);
+		hash *= 5;
+		hash += 0xe6546b64;
+	}
+
+	/* finalize */
+	hash ^= (uint32_t)count;
+	hash ^= hash >> 16;
+	hash *= 0x85ebca6b;
+	hash ^= hash >> 13;
+	hash *= 0xc2b2ae35;
+	hash ^= hash >> 16;
+	return (hash);
+}
+
+static uint32_t
+_hash(const void *data, size_t len)
+{
+	static uint32_t seed = 0xFEEDFACE;
+	
+	/*
+	 * Assume that if the first address is aligned, so is everything
+	 * else. We don't support differing data types in one hashmap.
+	 */
+	if (len >= 4 && len % 4 == 0 && ((uintptr_t)data & 3) == 0)
+		return (mm3_hash32(data, len/4, seed));
+
+	return (mm3_hash(data, len, seed));
+}
+
+dt_hashmap_t *
+dt_hashmap_create(size_t size)
+{
+	dt_hashmap_t *hm;
+
+	hm = malloc(sizeof(dt_hashmap_t));
+	if (hm == NULL)
+		return (NULL);
+
+	hm->dthm_size = size;
+	hm->dthm_nitems = 0;
+	hm->dthm_table = malloc(sizeof(dt_hashbucket_t) * hm->dthm_size);
+	if (hm->dthm_table == NULL) {
+		free(hm);
+		return (NULL);
+	}
+
+	memset(hm->dthm_table, 0, sizeof(void *) * hm->dthm_size);
+	return (hm);
+}
+
+void *
+dt_hashmap_lookup(dt_hashmap_t *hm, void *e, size_t es)
+{
+	uint32_t idx;
+
+	idx = _hash(e, es) % hm->dthm_size;
+
+	while (hm->dthm_table[idx].key != NULL &&
+	    memcmp(hm->dthm_table[idx].key, e, es) != 0) {
+		idx++;
+		idx %= hm->dthm_size;
+	}
+
+	return (hm->dthm_table[idx].data);
+}
+
+int
+dt_hashmap_insert(dt_hashmap_t *hm, void *e, size_t es, void *data)
+{
+	uint32_t idx;
+
+	idx = _hash(e, es) % hm->dthm_size;
+
+	while (hm->dthm_table[idx].key != NULL &&
+	    memcmp(hm->dthm_table[idx].key, e, es) != 0) {
+		idx++;
+		idx %= hm->dthm_size;
+	}
+
+	if (hm->dthm_table[idx].key == NULL) {
+		hm->dthm_nitems++;
+		hm->dthm_table[idx].data = data;
+		hm->dthm_table[idx].key = e;
+		hm->dthm_table[idx].keysize = es;
+	}
+	
+	return (0);
+}
+
+void *
+dt_hashmap_delete(dt_hashmap_t *hm, void *e, size_t es)
+{
+	uint32_t idx;
+	void *data;
+
+	idx = _hash(e, es) % hm->dthm_size;
+
+	while (hm->dthm_table[idx].key != NULL &&
+	    memcmp(hm->dthm_table[idx].key, e, es) != 0) {
+		idx++;
+		idx %= hm->dthm_size;
+	}
+
+	if (hm->dthm_table[idx].key == NULL)
+		return (NULL);
+
+	data = hm->dthm_table[idx].data;
+	hm->dthm_nitems--;
+	hm->dthm_table[idx].data = NULL;
+	hm->dthm_table[idx].key = NULL;
+	hm->dthm_table[idx].keysize = 0;
+
+	return (data);
+}
+
+void
+dt_hashmap_free(dt_hashmap_t *hm)
+{
+
+	free(hm->dthm_table);
+	free(hm);
+}
---
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_hashmap.h
@@ -0,0 +1,50 @@
+/*- 
+ * Copyright (c) 2021 Domagoj Stolfa <ds815@gmx.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef _DT_HASHMAP_H_
+#define _DT_HASHMAP_H_
+
+typedef struct {
+	void *data;
+	void *key;
+	size_t keysize;
+} dt_hashbucket_t;
+
+typedef struct dt_hashmap {
+	dt_hashbucket_t *dthm_table;
+	size_t          dthm_size;
+	size_t          dthm_nitems;
+} dt_hashmap_t;
+
+dt_hashmap_t *dt_hashmap_create(size_t);
+void *dt_hashmap_lookup(dt_hashmap_t *, void *, size_t);
+int dt_hashmap_insert(dt_hashmap_t *, void *, size_t, void *);
+void *dt_hashmap_delete(dt_hashmap_t *, void *, size_t);
+void dt_hashmap_free(dt_hashmap_t *);
+
+#endif /* _DT_HASHMAP_H_ */
