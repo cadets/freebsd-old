c7e13b6ce874e34e6c6b8c75f875c6148834d753
Domagoj Stolfa <ds815@gmx.com>
2021-09-18 17:57:30 +0100

    dtraced: Move dtraced connectivity bits to dtraced_connection.{c,h}.
    This commit moves all of the unix socket related things to a new file. The
    current state of the repository still does not compile.

---
+++ cddl/contrib/opensolaris/cmd/dtraced/dtraced_connection.h
@@ -0,0 +1,18 @@
+#ifndef _DTRACED_CONNECTION_H_
+#define _DTRACED_CONNECTION_H_
+
+#include <dt_list.h>
+
+struct dtd_fdlist {
+	dt_list_t list; /* next element */
+	int fd;         /* the actual filedesc */
+	int kind;       /* consumer/forwarder */
+	uint64_t subs;  /* events that efd subscribed to */
+};
+
+
+void *process_consumers(void *);
+int  setup_sockfd(struct dtd_state *);
+int  destroy_sockfd(struct dtd_state *);
+
+#endif // _DTRACED_CONNECTION_H_
