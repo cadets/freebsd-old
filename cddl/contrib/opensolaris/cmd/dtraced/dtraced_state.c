f13acfdb747b00aac6790eaeca9ba5b1ac30491b
Domagoj Stolfa <ds815@gmx.com>
2021-09-18 18:14:16 +0100

    dtraced: Move dtd_state-related things to dtraced_state.{c,h}.

---
+++ cddl/contrib/opensolaris/cmd/dtraced/dtraced_state.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "dtraced_errmsg.h"
+#include "dtraced_lock.h"
+#include "dtraced_state.h"
+
+int
+init_state(struct dtd_state *s)
+{
+	int err;
+
+	memset(s, 0, sizeof(struct dtd_state));
+	s->sockfd = -1;
+
+	if ((err = mutex_init(
+	    &s->socklistmtx, NULL, "socklist", CHECKOWNER_YES)) != 0) {
+		dump_errmsg("Failed to create sock list mutex: %m");
+		return (-1);
+	}
+
+	if ((err = mutex_init(
+	    &s->sockmtx, NULL, "socket", CHECKOWNER_YES)) != 0) {
+		dump_errmsg("Failed to create socket mutex: %m");
+		return (-1);
+	}
+
+	if ((err = mutex_init(
+	    &s->joblistcvmtx, NULL, "joblist condvar", CHECKOWNER_NO)) != 0) {
+		dump_errmsg("Failed to create joblist condvar mutex: %m");
+		return (-1);
+	}
+
+	if ((err = mutex_init(
+	    &s->joblistmtx, NULL, "joblist", CHECKOWNER_YES)) != 0) {
+		dump_errmsg("Failed to create joblist mutex: %m");
+		return (-1);
+	}
+
+	if ((err = mutex_init(
+	    &s->kill_listmtx, NULL, "kill list", CHECKOWNER_YES)) != 0) {
+		dump_errmsg("Failed to create kill list mutex: %m");
+		return (-1);
+	}
+
+	if ((err = mutex_init(
+	    &s->killcvmtx, NULL, "", CHECKOWNER_NO)) != 0) {
+		dump_errmsg("Failed to create kill condvar mutex: %m");
+		return (-1);
+	}
+
+	if ((err = mutex_init(
+	    &s->identlistmtx, NULL, "", CHECKOWNER_YES)) != 0) {
+		dump_errmsg("Failed to create identlist mutex: %m");
+		return (-1);
+	}
+
+	if ((err = pthread_cond_init(&s->killcv, NULL)) != 0) {
+		dump_errmsg("Failed to create kill list condvar: %m");
+		return (-1);
+	}
+
+	if ((err = pthread_cond_init(&s->joblistcv, NULL)) != 0) {
+		dump_errmsg("Failed to create joblist condvar: %m");
+		return (-1);
+	}
+
+	if (g_ctrlmachine == 0) {
+		s->dtt_fd = open("/dev/dttransport", O_RDWR);
+		if (s->dtt_fd == -1) {
+			dump_errmsg("Failed to open /dev/dttransport: %m");
+			return (-1);
+		}
+	}
+
+	s->outbounddir = dtd_mkdir(DTRACED_OUTBOUNDDIR, &process_outbound);
+	s->inbounddir = dtd_mkdir(DTRACED_INBOUNDDIR, &process_inbound);
+	s->basedir = dtd_mkdir(DTRACED_BASEDIR, &process_base);
+
+	s->outbounddir->state = s;
+	s->inbounddir->state = s;
+	s->basedir->state = s;
+
+	return (0);
+}
+
+int
+destroy_state(struct dtd_state *s)
+{
+	int err;
+	size_t i;
+	char *path;
+	struct dtd_joblist *j, *next;
+
+	LOCK(&s->joblistmtx);
+	for (j = dt_list_next(&s->joblist); j; j = next) {
+		next = dt_list_next(j);
+		free(j);
+	}
+	UNLOCK(&s->joblistmtx);
+
+	if ((err = mutex_destroy(&s->socklistmtx)) != 0) {
+		dump_errmsg("Failed to destroy sock list mutex: %m");
+		return (-1);
+	}
+
+	if ((err = mutex_destroy(&s->sockmtx)) != 0) {
+		dump_errmsg("Failed to destroy socket mutex: %m");
+		return (-1);
+	}
+
+	if ((err = mutex_destroy(&s->joblistcvmtx)) != 0) {
+		dump_errmsg("Failed to destroy joblist condvar mutex: %m");
+		return (-1);
+	}
+
+	if ((err = mutex_destroy(&s->joblistmtx)) != 0) {
+		dump_errmsg("Failed to destroy joblist mutex: %m");
+		return (-1);
+	}
+
+	if ((err = mutex_destroy(&s->kill_listmtx)) != 0) {
+		dump_errmsg("Failed to destroy kill list mutex: %m");
+		return (-1);
+	}
+
+	if ((err = mutex_destroy(&s->killcvmtx)) != 0) {
+		dump_errmsg("Failed to destroy kill list cv mutex: %m");
+		return (-1);
+	}
+
+	if ((err = pthread_cond_destroy(&s->killcv)) != 0) {
+		dump_errmsg("Failed to destroy kill condvar: %m");
+		return (-1);
+	}
+
+	if ((err = pthread_cond_destroy(&s->joblistcv)) != 0) {
+		dump_errmsg("Failed to destroy joblist condvar: %m");
+		return (-1);
+	}
+
+	dtd_closedir(s->outbounddir);
+	dtd_closedir(s->inbounddir);
+	dtd_closedir(s->basedir);
+
+	sem_destroy(&s->socksema);
+
+	destroy_sockfd(s);
+	s->sockfd = -1;
+
+	free(s->workers);
+
+	if (g_ctrlmachine == 0) {
+		close(s->dtt_fd);
+		s->dtt_fd = -1;
+	}
+
+	return (0);
+}
---
+++ cddl/contrib/opensolaris/cmd/dtraced/dtraced_state.h
@@ -0,0 +1,83 @@
+#ifndef _DTRACED_STATE_H_
+#define _DTRACED_STATE_H_
+
+#include <dt_list.h>
+#include <pthread.h>
+#include <stdatomic.h>
+
+#include "dtraced_directory.h"
+#include "dtraced_lock.h"
+
+/*
+ * dtraced state structure. This contains everything relevant to dtraced's
+ * state management, such as files that exist, connected sockets, etc.
+ */
+struct dtd_state {
+	dtd_dir_t *inbounddir;  /* /var/ddtrace/inbound */
+	dtd_dir_t *outbounddir; /* /var/ddtrace/outbound */
+	dtd_dir_t *basedir;     /* /var/ddtrace/base */
+
+	pthread_t inboundtd;    /* inbound monitoring thread */
+	pthread_t basetd;       /* base monitoring thread */
+	/* the outbound monitoring thread is the main thread */
+
+	/*
+	 * Sockets.
+	 */
+	mutex_t socklistmtx; /* mutex fos sockfds */
+	dt_list_t sockfds;   /* list of sockets we know about */
+
+	/*
+	 * Configuration socket.
+	 */
+	mutex_t sockmtx;  /* config socket mutex */
+	pthread_t socktd; /* config socket thread */
+	int sockfd;       /* config socket filedesc */
+	sem_t socksema;   /* config socket semaphore */
+
+	/*
+	 * dttransport fd and threads
+	 */
+	int dtt_fd;             /* dttransport filedesc */
+	pthread_t dtt_listentd; /* read() on dtt_fd */
+	pthread_t dtt_writetd;  /* write() on dtt_fd */
+
+	/*
+	 * Thread pool management.
+	 */
+	pthread_t *workers;       /* thread pool for the joblist */
+	mutex_t joblistcvmtx;     /* joblist condvar mutex */
+	pthread_cond_t joblistcv; /* joblist condvar */
+	mutex_t joblistmtx;       /* joblist mutex */
+	dt_list_t joblist;        /* the joblist itself */
+
+	/*
+	 * Children management.
+	 */
+	pthread_t killtd;      /* handle sending kill(SIGTERM) to the guest */
+	mutex_t kill_listmtx;  /* mutex of the kill list */
+	mutex_t killcvmtx;     /* kill list condvar mutex */
+	dt_list_t kill_list;   /* a list of pids to kill */
+	pthread_cond_t killcv; /* kill list condvar */
+	pthread_t reaptd;      /* handle reaping children */
+
+	/*
+	 * Consumer threads
+	 */
+	pthread_t consumer_listentd; /* handle consumer messages */
+	pthread_t consumer_writetd;  /* send messages to consumers */
+
+	_Atomic int shutdown;        /* shutdown flag */
+	int dirfd;                   /* /var/ddtrace */
+	int nosha;                   /* do we want to checksum? */
+	struct pidfh *pid_fileh;     /* lockfile */
+	int kq_hdl;                  /* event loop kqueue */
+
+	dt_list_t identlist;         /* list of identifiers */
+	mutex_t identlistmtx;        /* mutex protecting the ident list */
+};
+
+int init_state(struct dtd_state *);
+int destroy_state(struct dtd_state *);
+
+#endif // _DTRACED_STATE_H_
