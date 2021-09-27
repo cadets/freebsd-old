a4ee1597dd8898129d629c8547ab079e0fb2bdea
Domagoj Stolfa <ds815@gmx.com>
2021-09-27 20:02:08 +0100

    dtracedctl: Add new binary.

---
+++ cddl/contrib/opensolaris/cmd/dtracedctl/dtracedctl.c
@@ -0,0 +1,82 @@
+/*-
+ * Copyright (c) 2021 Domagoj Stolfa
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory (Department of Computer Science and
+ * Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
+ * DARPA SSITH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <dtraced.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static const char *program_name;
+
+enum {
+	CLEANUP_STATE = 1000,
+	HELP_PAGE,
+	SHOW_STATS
+};
+
+static struct option longopts[] = {
+	{ "cleanup-state",     optional_argument,    NULL,   	CLEANUP_STATE },
+	{ "help"         ,     no_argument      ,    NULL,      HELP_PAGE     },
+	{ "show-stats"   ,     optional_argument,    NULL,      SHOW_STATS    },
+};
+
+static void
+print_help(void)
+{
+	fprintf(stderr, "Usage: %s\n"
+	    "\t-c, --cleanup-state  clean state in VM (all if not specified).\n"
+	    "\t-h, --help           display this help page.\n"
+	    "\t-s, --show-stats     show statistics for a VM.\n",
+	    program_name);
+}
+
+int
+main(int argc, const char **argv)
+{
+	int ch;
+
+	program_name = argv[0];
+
+	while ((ch = getopt_long(argc, argv, "", longopts, NULL)) != -1) {
+		switch (ch) {
+		case HELP_PAGE:
+			print_help();
+			break;
+
+		case CLEANUP_STATE:
+			break;
+
+		case SHOW_STATS:
+			break;
+		}
+	}
+	return (0);
+}
