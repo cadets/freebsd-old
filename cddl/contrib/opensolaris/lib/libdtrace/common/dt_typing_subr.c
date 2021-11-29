4ba3404ba63d6e150e16c41e80ac68b4b890c784
Domagoj Stolfa <ds815@gmx.com>
2021-11-29 15:09:13 +0000

    dtrace: Fix getf() subtyping.

--- cddl/contrib/opensolaris/lib/libdtrace/common/dt_typing_subr.c
+++ cddl/contrib/opensolaris/lib/libdtrace/common/dt_typing_subr.c
@@ -1410,7 +1410,7 @@ dt_infer_type_subr(dt_ifg_node_t *n, dt_list_t *stack)
 		    (dt_typefile_t *[]) { dt_typefile_mod("D"),
 		                          dt_typefile_kernel() },
 		    2, "int", buf, sizeof(buf), subr_name[subr],
-		    SUBTYPE_FST | SUBTYPE_EQUAL);
+		    SUBTYPE_SND | SUBTYPE_EQUAL);
 
 		n->din_ctfid = dt_typefile_ctfid(n->din_tf, "file_t *");
 		if (n->din_ctfid == CTF_ERR)
