f66b6a84c33c0e832377e17be568e1dc409f94f6
Domagoj Stolfa <ds815@gmx.com>
2021-12-21 12:05:57 +0000

    dtracetsc: Add command line arguments and a help page.
    This commit adds the ability to dump csv output of the results.

--- usr.sbin/dtracetsc/dtracetsc.c
+++ usr.sbin/dtracetsc/dtracetsc.c
@@ -5,38 +5,91 @@
 
 #include <err.h>
 #include <errno.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sysexits.h>
 
-int
-main(void)
-{
-        dtrace_tscdata_t stackval;
-        dtrace_tscdata_t immstackval;
-        double stack, immstack;
-        size_t len;
+static const char *program_name;
 
-        len = sizeof(stackval);
-        if (sysctlbyname("kern.dtrace.stacktsc", &stackval,
-            &len, NULL, 0)) {
-		fprintf(stderr, "Failed to get stack values via sysctl: %s.",
-		    strerror(errno));
-                return (EX_OSERR);
+enum {
+	CSV_OUTPUT = 1000,
+	HELP_PAGE,
+};
+
+
+static struct option longopts[] = {
+	{ "csv"          ,     no_argument      ,    NULL,      CSV_OUTPUT    },
+	{ "help"         ,     no_argument      ,    NULL,      HELP_PAGE     },
+	{ NULL           ,     0                ,    NULL,      0             }
+};
+
+static void
+print_help(void)
+{
+	fprintf(stderr, "Usage: %s\n"
+	    "\t--csv   Show output in csv format. The output format order is:\n"
+	    "\t        (stack_time, immstack_time, cache_hit_rate).\n"
+	    "\t--help  display this help page.\n",
+	    program_name);
+}
+
+int
+main(int argc, char **argv)
+{
+	dtrace_tscdata_t stackval;
+	dtrace_tscdata_t immstackval;
+	double stack, immstack;
+	size_t len;
+	int csv = 0;
+	int ch;
+
+	program_name = argv[0];
+
+	while ((ch = getopt_long(argc, (char *const *)argv,
+	    "", longopts, NULL)) != -1) {
+		switch (ch) {
+		case 0:
+			break;
+
+		case HELP_PAGE:
+			print_help();
+			return (EX_OK);
+
+		case CSV_OUTPUT:
+			csv = 1;
+			break;
+
+		default:
+			print_help();
+			return (EXIT_FAILURE);
+		}
 	}
 
-        if (sysctlbyname("kern.dtrace.immstacktsc", &immstackval,
-            &len, NULL, 0)) {
+	len = sizeof(stackval);
+	if (sysctlbyname("kern.dtrace.stacktsc", &stackval, &len, NULL, 0)) {
 		fprintf(stderr, "Failed to get stack values via sysctl: %s.",
 		    strerror(errno));
 		return (EX_OSERR);
 	}
 
-        stack = ((double)stackval.sum) / ((double)stackval.cnt) / 1000000;
-        immstack = ((double)immstackval.sum) / ((double)immstackval.cnt) / 1000000;
+	if (sysctlbyname(
+		"kern.dtrace.immstacktsc", &immstackval, &len, NULL, 0)) {
+		fprintf(stderr, "Failed to get stack values via sysctl: %s.",
+		    strerror(errno));
+		return (EX_OSERR);
+	}
 
-        printf("Stack: %lf\nImmstack: %lf\n", stack, immstack);
+	stack = ((double)stackval.sum) / ((double)stackval.cnt) / 1000000;
+	immstack = ((double)immstackval.sum) / ((double)immstackval.cnt) /
+	    1000000;
 
-        return (EXIT_SUCCESS);
+	if (csv != 0) {
+		printf("%lf, %lf, %lf\n", stack, immstack, 0.0f);
+	} else {
+		printf("Stack: %lf\nImmstack: %lf\n", stack, immstack);
+	}
+
+	return (EXIT_SUCCESS);
 }
